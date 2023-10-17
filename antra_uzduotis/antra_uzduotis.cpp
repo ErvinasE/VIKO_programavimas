1ce6f152b2b477fd437c9122e4bd828ac6d73ad0
Ervinas Eismontas <ervinas.eismontas@gmail.com>
2023-10-17 23:02:02 +0300

    antra_uzduotis.cpp

--- antra_uzduotis/antra_uzduotis.cpp
+++ antra_uzduotis/antra_uzduotis.cpp
@@ -8,9 +8,13 @@ int main()
 
     string raktas, tekstas;
 
-    string tekstas_str;
+    string tekstas_str[50];
 
     int indeksas_tekstas, indeksas_abecele;
+
+    int mod = 0;
+
+    int num = 0;
 
     string abecele_lt[32] = {"a", "ą", "b", "c", "č", "d", "e", "ę", "ė", "f", "g", "h", 
     "i", "į", "y", "j", "k", "l", "m", "n", "o", "p", "r", "s", "š", "t", "u", "ų", "ū",
@@ -19,19 +23,33 @@ int main()
 
 
     cout << "Iveskite teksta" << endl;
+    // cin >> tekstas;
     cin >> tekstas;
-    cout << "Iveskite  rakta" << endl;
-    cin >> raktas;
 
-    for (int i = 0;i< tekstas.size();i++)
+
+
+    for (int i = 0;i<= tekstas.size();i++)
     {
+        cout << i << endl;
+        tekstas_str[i] = tekstas[i];
         for (int j = 0;j <= 32;j++)
         {
-            
+            if (tekstas_str[i] == abecele_lt[j])
+            {
+                cout << "Skaicius :" << j << " raide: " << abecele_lt[j];
+                cout << endl;
 
+                num++;
+                if (num == 1)
+                {
+                    
+                    break;
+                }
+
+
+            }
         }
     }
-
 
     
 
@@ -39,4 +57,4 @@ int main()
 
 
     return 0;
-}
+} 
