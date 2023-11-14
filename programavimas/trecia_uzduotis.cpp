011a795769e15ac79f4351d0611fa52c31c851ad
Ervinas Eismontas <ervinas.eismontas@gmail.com>
2023-11-14 23:47:28 +0200

    balls

--- programavimas/trecia_uzduotis.cpp
+++ programavimas/trecia_uzduotis.cpp
@@ -33,41 +33,8 @@ bool balses()
 }
 int dbd(int sk1, int sk2)
 {
-    int liek = 1;
-    int dbd = 0;
-    int dal1, dal2;
-    if ( sk1 > sk2)
-    {
-        dal1 = sk1;
-        dal2 = sk2;
-    }
-    else 
-    {
-        dal1 = sk1;
-        dal2 = sk2;
-    }
-
-
-
-    while (liek >= 0)
-    {   
-        liek = dal1 % dal2;
-        dal2 = dal1;
-        dal2 = liek;
-        cout << liek << endl;
-        if (sk1 == 0 && liek == 0 && (dal1 / dal2 > 0))
-        {   
-            dal1 % dal2; 
-            cout << "DBD: " << dbd << endl;
-        }
-        else if (sk2 == 0)
-        {   dbd = sk1;
-            cout << "DBD: " << dbd << endl;
-        }
-    }
-
+    
 }
-
 int main ()
 {
     int sk;
