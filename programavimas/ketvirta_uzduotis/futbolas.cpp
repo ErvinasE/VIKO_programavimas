8663a275491ce09e311407e954c3b338f58dfc8b
Ervinas Eismontas <ervinas.eismontas@gmail.com>
2023-12-18 22:09:46 +0200

    penkta uzduotis

--- programavimas/ketvirta_uzduotis/futbolas.cpp
+++ programavimas/ketvirta_uzduotis/futbolas.cpp
@@ -7,16 +7,20 @@ using namespace std;
 
 int main ()
 {   
-    double kaina, pardBilietai, suma;
+    double kaina, pardBilietai, suma, bsuma;
     ifstream file;
     file.open("bilietai.txt");
+    bsuma = 0;
     for (int i = 0;i<4;i++)
     {
         file>>kaina>>pardBilietai;
         cout << fixed << setprecision(0)<<" Bielietu skaicius : " << pardBilietai;
         suma = pardBilietai * kaina;
         cout << fixed << setprecision(2)<<" Bendra pardavimu suma :" << suma << endl;
+        bsuma = bsuma + suma;
+        // cout << fixed << setprecision(2)<<" Visa pardavimu suma :" << bsuma << endl;
     }
+    cout << fixed << setprecision(2)<<" Visa pardavimu suma :" << bsuma << endl;
 
     file.close();
     return 0;
---
+++ programavimas/penkta_uzduotis/restoranas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
