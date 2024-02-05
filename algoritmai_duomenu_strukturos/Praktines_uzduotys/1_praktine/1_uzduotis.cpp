3ae5b121748fa148f1e31c630727e7ba29796c91
Ervinas Eismontas <ervinas.eismontas@gmail.com>
2024-02-05 21:19:26 +0200

    vorp

---
+++ algoritmai_duomenu_strukturos/Praktines_uzduotys/1_praktine/1_uzduotis.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+int main ()
+{   int temp1 = 0, temp2 = 0;
+    int nsuma = 0;
+    int melementas, delementas;
+    int mas[7] = {1, 6, 10, 2, -2, -6, -7};
+    int mas2[7];
+    for (int i = 0; i<=7;i++)
+    {
+        // skaiciuojame neigiamu elemntu suma
+        if ( mas[i] < 0)
+        {
+            nsuma = nsuma + mas[i];
+        }
+    }
+
+    for (int j = 0;j<7;j++)
+    {
+        
+    }
+    // cout << "Neigiamu skaiciu suma "<< nsuma << endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+}
