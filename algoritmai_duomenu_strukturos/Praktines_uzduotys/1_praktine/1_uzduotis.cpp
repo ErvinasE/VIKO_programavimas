caac9ff33b8700772ebbd792107312367960b67b
Ervinas Eismontas <ervinas.eismontas@gmail.com>
2024-02-06 00:24:08 +0200

    beveik pabaigta 1 uzdavinys

--- algoritmai_duomenu_strukturos/Praktines_uzduotys/1_praktine/1_uzduotis.cpp
+++ algoritmai_duomenu_strukturos/Praktines_uzduotys/1_praktine/1_uzduotis.cpp
@@ -3,12 +3,13 @@
 
 using namespace std;
 
-
+int l = 0;
+int z = 0;
 int main ()
-{   int temp1 = 0, temp2 = 0;
-    int nsuma = 0;
+{   int nsuma = 0;
+    int temp;
     int melementas, delementas;
-    int mas[7] = {1, 6, 10, 2, -2, -6, -7};
+    int mas[7] = {1, 6, 120, 2, -2, -6, -7};
     int mas2[7];
     for (int i = 0; i<=7;i++)
     {
@@ -18,12 +19,48 @@ int main ()
             nsuma = nsuma + mas[i];
         }
     }
-
-    for (int j = 0;j<7;j++)
+        // didziausio elemento radimas
+        delementas = mas[0];
+    for (l = 0;l<7;l++)
     {
+        if (delementas < mas[l])
+        {
+            delementas = mas[l];
+        }
+    }
+        // maziausio elemento radimas
+        melementas = mas[0];
+    for (int i = 0;i<7;i++)
+    {
+        if (melementas > mas[i])
+        {
+            melementas = mas[i];
+        }
+
+    }
+        // rikiavimas elementu nuo maziausio iki didziausio
+    for (int j = 0;j<7;j++)
+    {   
+        for (int k = 0;k<7;k++)
+        {
+            if (mas[k] > mas[k+1])
+            {
+                temp = mas[k];
+                mas[k] = mas[k+1];
+                mas[k+1] = temp;
+            }
+        }
         
     }
-    // cout << "Neigiamu skaiciu suma "<< nsuma << endl;
+
+    for (int i = 0;i< 7;i++)
+    {
+        cout << mas[i] << ",";
+    }
+    cout << "Neigiamu skaiciu suma "<< nsuma << endl;
+    cout << "Didziausias skaicius : " << delementas << endl;
+    cout << "Maziausias skaicius : " << melementas << endl;
+
 
 
 
